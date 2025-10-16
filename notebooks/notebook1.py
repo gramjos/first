aa7841ed2f7355b3663d3c641195ac9ea89c4f4b
gramjos <graham.joss@gmail.com>
2025-10-16 15:29:06 -0500

    quick update

---
+++ notebooks/notebook1.py
@@ -0,0 +1,25 @@
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#     "geopandas[all]",
+# ]
+# ///
+import marimo
+
+__generated_with = "0.17.0"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import geopandas
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
